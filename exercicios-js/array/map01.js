9fa87c697b863d533cdf354c657c8d0d3414463f
Flávia Reis <flaviarster@gmail.com>
2023-12-26 15:36:01 -0300

    Update map01.js

--- exercicios-js/array/map01.js
+++ exercicios-js/array/map01.js
@@ -1,5 +1,14 @@
 const nums = [1, 2, 3, 4, 5]
 
-const dobro = nums.map(num => num*2)
+const dobro = nums.map(num => num * 2)
 
 console.log(dobro)
+
+const soma10 = num => num + 10
+const triplo = num => num * 3
+const dinheiro = num => `R$ ${parseFloat(num).toFixed(2).replace('.', ',')}`
+
+const resultado = nums.map(soma10).map(triplo).map(dinheiro)
+
+console.log(resultado)
+
