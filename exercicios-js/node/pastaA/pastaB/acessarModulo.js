7e700dc1fb5021149b0e51e39f054f470e6edde9
Flávia Reis <flaviarster@gmail.com>
2024-01-17 08:13:46 -0300

    Create acessarModulo.js

---
+++ exercicios-js/node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,6 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const moduloB = require('/Users/flavi/OneDrive/Documentos/Repositorios/Estudos/exercicios-js/node/moduloB')
+console.log(moduloB)
+
